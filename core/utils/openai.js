3e3338f50f21a6c9cb3ecb81b5fa51422cf8ab95
Aura <fazrihafidz120@gmail.com>
2025-06-21 14:16:58 +0700

    REVISI AI

--- core/utils/openai.js
+++ core/utils/openai.js
@@ -4,16 +4,7 @@ require('dotenv').config();
 const models = [
   "deepseek/deepseek-r1-0528:free", 
   "mistralai/mistral-7b-instruct:free",
-  "openchat/openchat-3.5:free",
-  "deepseek/deepseek-coder:free",
-  "undi95/toppy-m-7b:free",
-   "gryphe/mythomax-l2-13b:free",
-   "nousresearch/nous-capybara-7b:free",              
-  "open-orca/mistral-7b-openorca:free",             
-  "huggingfaceh4/zephyr-7b-beta:free",              
-  "cognitivecomputations/dolphin-mixtral-8x7b:free", 
-  "mancer/weaver:free",                              
-  "openrouter/cinematika-7b:free" 
+  "deepseek/deepseek-coder:free"
 ]
 let lastSuccessfulModel = null
 async function askOpenAI(userHistory = []) {

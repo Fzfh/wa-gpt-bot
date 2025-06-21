1e49fa8f5a8bcc0c5aeaa7d4465b1770d4177a78
AURA <fazrihafidz120@gmail.com>
2025-06-21 20:18:12 +0700

    Update botresponse.js

--- core/botresponse.js
+++ core/botresponse.js
@@ -209,8 +209,12 @@ if (text === '.reset') {
 }
 
 function registerGroupUpdateListener(sock) {
-  sock.ev.removeAllListeners('group-participants.update')
+  sock.ev.removeAllListeners('group-participants.update') 
+  sock.ev.on('group-participants.update', async (update) => {
+    await handleWelcome(sock, update) 
+  })
 }
+
 module.exports = {
   handleResponder,
   greetedUsers,

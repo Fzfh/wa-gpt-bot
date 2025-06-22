2e1aa3dec50cf6e077c3071919466f3e6d5474e3
AURA <fazrihafidz120@gmail.com>
2025-06-22 23:59:09 +0700

    Update state.js

--- core/state.js
+++ core/state.js
@@ -1,6 +1,8 @@
 const produkMap = new Map()
 const selectedNominalMap = new Map()
 const lastCommandMap = new Map()
+const selectedKoutaNominalMap = new Map()
+const lastKoutaCommandMap = new Map()
 
 module.exports = {
   produkMap,

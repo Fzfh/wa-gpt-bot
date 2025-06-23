130b9f0d3f7ee34cabf12dab020ff17bb138569d
AURA <fazrihafidz120@gmail.com>
2025-06-23 18:38:31 +0700

    Update state.js

--- core/state.js
+++ core/state.js
@@ -1,16 +1,17 @@
+// State Global (pastikan shared antar modul)
 global.produkPulsaMap = global.produkPulsaMap || new Map()
 global.selectedPulsaMap = global.selectedPulsaMap || new Map()
 global.lastPulsaMap = global.lastPulsaMap || new Map()
 
-const produkKoutaMap = new Map()
-const selectedKoutaMap = new Map()
-const lastKoutaMap = new Map()
+global.produkKoutaMap = global.produkKoutaMap || new Map()
+global.selectedKoutaMap = global.selectedKoutaMap || new Map()
+global.lastKoutaMap = global.lastKoutaMap || new Map()
 
 module.exports = {
   produkPulsaMap: global.produkPulsaMap,
   selectedPulsaMap: global.selectedPulsaMap,
   lastPulsaMap: global.lastPulsaMap,
-  produkKoutaMap,
-  selectedKoutaMap,
-  lastKoutaMap
+  produkKoutaMap: global.produkKoutaMap,
+  selectedKoutaMap: global.selectedKoutaMap,
+  lastKoutaMap: global.lastKoutaMap
 }

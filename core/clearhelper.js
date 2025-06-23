9c93ca41ef97486d5375d9ec13a41519e29cc1c7
AURA <fazrihafidz120@gmail.com>
2025-06-23 18:38:11 +0700

    Update clearhelper.js

--- core/clearhelper.js
+++ core/clearhelper.js
@@ -5,22 +5,23 @@ const {
   produkKoutaMap,
   selectedKoutaMap,
   lastKoutaMap
-} = require('./state');
+} = require('./state')
 
 function clearPulsaSession(userId) {
+  produkPulsaMap.delete(userId)
+  selectedPulsaMap.delete(userId)
+  lastPulsaMap.delete(userId)
   console.log('🧹 clearPulsaSession for:', userId)
-  produkPulsaMap.delete(userId);
-  selectedPulsaMap.delete(userId);
-  lastPulsaMap.delete(userId);
 }
 
 function clearKoutaSession(userId) {
-  produkKoutaMap.delete(userId);
-  selectedKoutaMap.delete(userId);
-  lastKoutaMap.delete(userId);
+  produkKoutaMap.delete(userId)
+  selectedKoutaMap.delete(userId)
+  lastKoutaMap.delete(userId)
+  console.log('🧹 clearKoutaSession for:', userId)
 }
 
 module.exports = {
   clearPulsaSession,
   clearKoutaSession
-};
+}

9ca9c8b7f7fb5262e9e8a8fa881174a7ea69d847
AURA <fazrihafidz120@gmail.com>
2025-06-25 21:54:23 +0700

    Update tiktokDownloader.js

--- commands/tiktokDownloader.js
+++ commands/tiktokDownloader.js
@@ -5,16 +5,17 @@ async function downloadTiktok(url) {
     const res = await axios.get(`https://tikwm.com/api/?url=${encodeURIComponent(url)}`);
     const data = res.data.data;
 
-    if (data && data.play) {
-      return {
-        videoUrl: data.play,
-        musicUrl: data.music,
-        title: data.title,
-        cover: data.cover
-      };
-    } else {
-      throw new Error('Gagal ambil data');
-    }
+    if (!data) throw new Error('Gagal ambil data');
+
+    return {
+      isPhoto: Array.isArray(data.images) && data.images.length > 0,
+      images: data.images || [],
+      videoUrl: data.play || null,
+      musicUrl: data.music || null,
+      title: data.title,
+      author: data.author?.nickname || '',
+      cover: data.cover
+    };
   } catch (e) {
     console.error('Download Error:', e);
     return null;

68cd443618263e797eb03b0e67df3e3302ca5538
AURA <fazrihafidz120@gmail.com>
2025-06-29 15:42:52 +0700

    Update botresponse.js

--- core/botresponse.js
+++ core/botresponse.js
@@ -27,7 +27,7 @@ const hapusProduk = require('../commands/hapusProduk');
 const tambahProduk = require('../commands/tambahProduk');
 const downloadTiktok = require('../commands/tiktokDownloader');
 const downloadInstagram = require('../commands/igDownloader');
-const downloadYoutube = require('../commands/youtubeDownloader');
+const downloadYouTubeVideo = require('../commands/youtubeDownloader');;
 const sendAll = require('../commands/sendAll');
 
 const greetedUsers = new Set()

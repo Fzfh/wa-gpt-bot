fa78c8518d401bbb52ef6b6c895e4a06f80d536d
AURA <fazrihafidz120@gmail.com>
2025-07-08 21:57:15 +0700

    UPDATE AURABOT V1.0.0

--- core/stickerHelper.js
+++ core/stickerHelper.js
@@ -118,7 +118,7 @@ function wrapText(ctx, text, maxWidth) {
 
 // 🛠️ ALIGN TENGAH teks sticker
 async function createStickerFromText(text) {
-  const W = 512, H = 512, fontSize = 120, pad = 30;
+  const W = 512, H = 512, fontSize = 120, pad = 40;
   const canvas = createCanvas(W, H);
   const ctx = canvas.getContext('2d');
   ctx.fillStyle = 'white';
@@ -181,7 +181,7 @@ async function createStickerFromText(text) {
     pack: 'AuraBot',
     author: 'AURA',
     type: StickerTypes.DEFAULT,
-    quality: 10
+    quality: 5
   });
   return sticker.toBuffer();
 }
